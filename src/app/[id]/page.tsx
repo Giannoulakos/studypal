9475602c7bf6f39c1adc21077d5dfd7d99c5a148
Giannis Giannoulakos <yiannoulakos1@gmail.com>
2024-09-08 18:49:48 +0300

    Popover for keyboard

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -11,9 +11,19 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from '@/components/ui/pagination';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+
 import { useState } from 'react';
 import { Document, Page } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 import { pdfjs } from 'react-pdf';
+import Keyboard from '@/components/ui/mathkeyboard/keyboard';
+import { Button } from '@/components/ui/button';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -26,6 +36,8 @@ export default function StudyPage() {
   const [numPagesArr, setNumPagesArr] = useState<number[]>([0]);
   const [pageNumber, setPageNumber] = useState<number>(0);
 
+  const [text, setText] = useState<string>('');
+
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     let tmpArr: number[] = [];
     for (let i = 1; i <= numPages; i++) {
@@ -37,7 +49,7 @@ export default function StudyPage() {
 
   return (
     <main className='flex h-full min-h-screen'>
-      <div className=' min-w-[15vw] h-screen basis-1/2 bg-[white] p-10 '>
+      <div className=' min-w-[15vw] min-h-screen basis-1/2 bg-[white] p-10 '>
         <div className='mb-10'>
           <InputFile onData={(file) => setDoc(file)} />
         </div>
@@ -80,15 +92,29 @@ export default function StudyPage() {
           </PaginationContent>
         </Pagination>
         {doc && (
-          <div className='mt-10 h-fit '>
+          <div className='mt-10 h-screen flex justify-center'>
             <Document file={doc} onLoadSuccess={onDocumentLoadSuccess}>
               <Page pageNumber={pageNumber} />
             </Document>
           </div>
         )}
       </div>
-      <div className='flex justify-center min-w-[25vw] basis-1/2 p-10 min-h-screen h-full bg-[whitesmoke]'>
-        <Textarea className='w-4/5 ' />
+      <div className='flex justify-center min-w-[25vw] basis-1/2 p-10 min-h-screen bg-[whitesmoke]'>
+        <Textarea
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          className='w-4/5 '
+        />
+      </div>
+      <div className='fixed right-10 top-10'>
+        <Popover>
+          <PopoverTrigger>
+            <Button>Math</Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <Keyboard onKeyPress={(key) => setText(text + key)} />
+          </PopoverContent>
+        </Popover>
       </div>
     </main>
   );
