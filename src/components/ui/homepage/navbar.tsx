6b4795da93fbd4a7bd18ba0b91abf5b81ce54a6e
Giannis Giannoulakos <yiannoulakos1@gmail.com>
2024-09-18 20:03:51 +0300

    added navbar signup and logout

--- src/components/ui/homepage/navbar.tsx
+++ src/components/ui/homepage/navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -8,8 +10,28 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from '@/components/ui/navigation-menu';
+import { Button } from '../button';
+import { createClient } from '../../../../utils/supabase/client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
+  const supabase = createClient();
+  const router = useRouter();
+
+  const [user, setUser] = useState<any>(null);
+
+  const authenticate = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    setUser(user);
+  };
+
+  useEffect(() => {
+    authenticate();
+  }, []);
+
   return (
     <nav className='flex items-center justify-between 5'>
       <div className=' font-semibold text-2xl'>StudyPal</div>
@@ -35,6 +57,31 @@ const Navbar = () => {
           </NavigationMenuLink>
         </NavigationMenuList>
       </NavigationMenu>
+      <div>
+        {user ? (
+          <Button
+            onClick={async (e: any) => {
+              e.preventDefault();
+              const { error } = await supabase.auth.signOut();
+              if (error)
+                return console.log('Error logging out:', error.message);
+              setUser(null);
+            }}
+            variant={'link'}
+            className='text-base'
+          >
+            Logout
+          </Button>
+        ) : (
+          <Button
+            onClick={() => router.push('/login')}
+            variant={'link'}
+            className='text-base'
+          >
+            Sign Up
+          </Button>
+        )}
+      </div>
     </nav>
   );
 };
